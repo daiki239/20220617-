a282960b5fb87c2a66f2ee420256ab47079876e0
daiki239 <ghiuuv@gmail.com>
2022-06-28 15:19:16 +0900

    10

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,6 @@
 const target = document.getElementById("drawer__menu");
 target.addEventListener('click', () => {
+   const target = document.getElementById("drawer__menu");
   target.classList.toggle('open');
   const nav = document.getElementById("drawer__nav");
   nav.classList.toggle('in');
